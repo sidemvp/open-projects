37955f19661ff6e6496322bc316d48a863a18f88
Bao D <baocancode@gmail.com>
2021-09-10 17:29:27 +1000

    project-listing: Add app page

---
+++ app/project-listing/src/components/app-page.tsx
@@ -0,0 +1,26 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+
+import { AppHead } from 'mvp-common-components/src/app-head'
+import { createAppTheme } from 'mvp-common-components/src/app-theme'
+import { useOrientationEffect } from 'mvp-common-components/src/window-hooks'
+
+const appTheme = createAppTheme({
+  background: 'gray',
+  primary: 'teal',
+})
+
+const useAppPage = () => {
+  useOrientationEffect()
+}
+
+export const AppPage = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useAppPage()
+
+  return (
+    <ChakraProvider theme={appTheme}>
+      <AppHead />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
---
+++ app/project-listing/src/pages/_app.ts
@@ -0,0 +1,3 @@
+import { AppPage } from 'mvp-app-project-listing/src/components/app-page'
+
+export default AppPage
