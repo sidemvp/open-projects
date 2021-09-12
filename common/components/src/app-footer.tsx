fdb44ad8e428f727d978f89739c170dd8b42d40f
Bao D <baocancode@gmail.com>
2021-09-12 10:42:54 +1000

    Resolve footer url by environment

--- common/components/src/app-footer.tsx
+++ common/components/src/app-footer.tsx
@@ -3,10 +3,12 @@ import { FunctionComponent } from 'react'
 import { AiOutlineCoffee } from 'react-icons/ai'
 
 export const AppFooter: FunctionComponent = () => {
+  const envDomain = process.env.VERCEL_ENV === 'preview' ? 'demo' : 'www'
+
   return (
     <Text fontSize='sm' fontWeight='medium' textAlign='center' paddingY={16}>
       Made with <Icon as={AiOutlineCoffee} boxSize={6} marginBottom={2} /> by{' '}
-      <Link href='https://www.sidemvp.com' color='primary.400' isExternal>
+      <Link href={`https://${envDomain}.sidemvp.com`} color='primary.400' isExternal>
         SideMVP
       </Link>
     </Text>
